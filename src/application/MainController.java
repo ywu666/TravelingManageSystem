5ec02201228c5908911c724321f6d587ee990165
ywu666 <ywu660@aucklanduni.ac.nz>
2019-10-03 10:18:48 +1300

    Initialised user inerface model

---
+++ src/application/MainController.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+	
+	@FXML Button sale;
+	@FXML Button product;
+	@FXML Button costumer;
+	@FXML Button employess;
+	
+	@FXML
+	private void handleCostumers{
+		
+	}
+	
+	@FXML
+	private void handleProducts{
+		
+	}
+	
+	@FXML
+	private void handleSales{
+		
+	}
+	
+	@FXML
+	private void handleEmployess{
+		
+	}
+
+}
